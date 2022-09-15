1591f2dd88b189bd6c5dce941f0930c73040c410
Arthur Roberto <arthurt520@hotmail.com>
2022-09-14 21:04:26 -0300

    Projeto js commitado

---
+++ script.js
@@ -0,0 +1,16 @@
+function calcularmedia(){
+    var num1 = parseInt(document.getElementById("num1").value)
+    var num2 = parseInt(document.getElementById("num2").value)
+    var num3 = parseInt(document.getElementById("num3").value)
+
+    var media = (num1 + num2 + num3) / 3
+
+   document.getElementById("resultado").innerHTML = "Resultado: " + media 
+
+
+
+}
+
+
+
+
